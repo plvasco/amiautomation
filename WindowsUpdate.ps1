7cc128d634f565093b0ccd4ad0dc7bace508abec
plvasco <35873800+plvasco@users.noreply.github.com>
2019-07-30 16:31:06 -0500

    Add files via upload

---
+++ WindowsUpdate.ps1
@@ -0,0 +1,4 @@
+Install-Module PSWindowsUpdate
+Get-Command –module PSWindowsUpdate
+Add-WUServiceManager -ServiceID 7971f918-a847-4430-9279-4a52d1efe18d
+Get-WUInstall –MicrosoftUpdate –AcceptAll –AutoReboot
