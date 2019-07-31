a16e608a3fd8c8c596a85acdd932af1dc4a0e129
plvasco <35873800+plvasco@users.noreply.github.com>
2019-07-31 14:57:31 -0500

    Update prereqs.ps1

--- prereqs.ps1
+++ prereqs.ps1
@@ -1,7 +1,7 @@
 ﻿# Your account access key - must have read access to your S3 Bucket
-$accessKey = "AKIAW6GYIYEDWWOXQ63Z"
+$accessKey = "blank"
 # Your account secret access key
-$secretKey = "iMZVIIa0lK5fyhxzaJiK8c+AbGHDSidfbmUUohwa"
+$secretKey = "blank"
 # The region associated with your bucket e.g. eu-west-1, us-east-1 etc. (see http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-regions-availability-zones.html#concepts-regions)
 $region = "us-gov-west-1"
 # The name of your S3 Bucket
